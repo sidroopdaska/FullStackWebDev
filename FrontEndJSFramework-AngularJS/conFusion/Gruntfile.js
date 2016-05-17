d0dfcfe223494a082d8c45dbb2953092af1f8554
Siddharth <sid3637@gmail.com>
2016-05-18 00:03:19 +0100

    Added JSHint Task

--- FrontEndJSFramework-AngularJS/conFusion/Gruntfile.js
+++ FrontEndJSFramework-AngularJS/conFusion/Gruntfile.js
@@ -1,9 +1,34 @@
 'use strict';
 
 module.exports = function(grunt){
-      
     
-    grunt.initConfig({});
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+    
+    // Automatically load required Grunt tasks. Looks inside node_modules folder and automatically loads grunt modules when required in the Gruntfile.js. Other alternative, explicitly include all the grunt modules.
+    require('jit-grunt')(grunt); 
+    
+    // Define configuation for all the Grunt modules
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Make sure code styles are up to par and there are no obvious mistakes
+        jshint: {
+          options: {
+            jshintrc: '.jshintrc',
+            reporter: require('jshint-stylish')
+          },
+          all: {
+            src: [
+              'Gruntfile.js',
+              'app/scripts/{,*/}*.js'
+            ]
+          }
+        }
+    });
+
+    grunt.registerTask('build', ['jshint']);
+    grunt.registerTask('default',['build']);
     
     
 };
